c6ee70218cc4231ce7c65234daef6cc5d8325824
Leontiev Ramalho <leontievramalho@gmail.com>
2023-10-11 16:54:27 -0300

    incompleto

--- main.cpp
+++ main.cpp
@@ -6,10 +6,32 @@ struct no{
 	no *next;
 };
 
+struct noD{
+	int data;
+	noD *next;
+	noD *prev;
+};
+
+
 void printList(no *&head){
 	no *atual = head;
 	cout << "[";
 	while(atual != NULL){
+		cout << atual -> data << " "; 
+		atual = atual -> next;
+	}
+	cout << "]" << endl;
+}
+
+
+void printCircleListTwoTimes(no *&head){
+	no *atual = head;
+	int count = 0;
+	cout << "[";
+	while(count<2){
+		if(atual -> next == head){
+			count ++;
+		}
 		cout << atual -> data << " "; 
 		atual = atual -> next;
 	}
@@ -23,6 +45,25 @@ int tamanhoList(no *head){
 		head = head -> next;	
 	}
 	return count;
+}
+
+int meioList(no *&head){
+	no *atual = head;
+	int count = 0;
+	int meioIndex = (tamanhoList(head)-1)/2;
+	while(count < meioIndex){
+		count ++;
+		atual = atual -> next;
+	}
+	return atual -> data;
+}
+
+void circleList(no *&head){
+	no *tail = head;
+	while(tail -> next != NULL){
+		tail = tail -> next;
+	}
+	tail -> next = head;
 }
 
 void removerList(no *&head, int pos){
@@ -45,8 +86,115 @@ void removerList(no *&head, int pos){
 	}
 }
 
-int main() {
+void printListD(noD *&head){
+	noD *atual = head;
+	cout << "[";
+	while(atual != NULL){
+		cout << atual -> data << " "; 
+		atual = atual -> next;
+	}
+	cout << "]" << endl;
+}
+
+int maiorNoD(noD *&head){
+	noD *atual = head;
+	int maior = head -> data;
+	while(atual != NULL){
+		if(atual -> data > maior){
+			maior = atual -> data;
+		}
+		atual = atual -> next;
+	}
+	return maior;
+}
+
+int tamanhoListD(noD *head){
+	int count = 0;
+	while(head != NULL){
+		count ++;
+		head = head -> next;	
+	}
+	return count;
+}
+
+void inserirListD(noD *&head, noD *novoNo, int pos){
+	int count = 0;
+	noD *atual = head;
+	while(atual->next != NULL && count < pos){
+		count ++;
+		atual = atual -> next;
+	}
+	if(count != pos && count+1 != pos){
+		cout << "Nao eh possivel acrescentar, porque essa posicao esta muito alem." << endl;
+	}else if(count == 0){
+		novoNo -> next = head;
+		novoNo -> prev = NULL;
+		head -> prev = novoNo;
+	}else{
+		noD *anterior = atual -> prev;
+		novoNo -> prev = anterior;
+		anterior -> next = novoNo;
+		atual -> prev = novoNo;
+		novoNo -> next = atual;
+	}
+}
+
+noD encontrarD(noD *&head, int pos){
+	int count = 0;
+	noD *atual = head;
+	while(atual->next != NULL && count < pos){
+		count ++;
+		atual = atual -> next;
+	}
+	if(count == pos){
+		return atual;
+	}
+	return NULL;
+}
+
+int mergeListD(noD *&head, int inicio, int final){
+	while(inicio<final){
+		int meio = (inicio + final)/2;
+		noD *esquerda = head;
+		noD *fimEsquerda = encontrarD(meio);
+		noD *direita = fimEsquerda -> next;
+		fimEsquerda -> NULL
+		direita -> prev = NULL; 
+		
+		mergeListD(esquerda, inicio, meio);
+		mergeListD(direita, meio + 1, final);
+		
+		merge(head, esquerda, direita, inicio, meio, final);
+	}
+}
+
+void merge(noD *&head, noD *esquerda, noD *direita, int inicio, int meio, int final){
+	int tamEsq = tamanhoListD(esquerda);
+	int tamDir = tamanhoListD(direita);
+	int tam = tamEsq + tamDir;
 	
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	
+	while(i < tamEsq && j < tamDir){
+		if(encontrarD(esquerda, i)<=encontrarD(direita, j)){
+			inserirListD(head, encontrarD(esquerda, i), countMerge);
+			countMerge ++;
+			i++
+			k++
+		} else{
+			inserirListD(head, encontrarD(direita, j),countMerge);
+			countMerge ++;
+			j++
+			k++
+		}
+	}
+	//terminar
+}
+
+int main() {
+	countMerge = 0;
 	// criando lista
 	no *lista = new no;
 	lista -> data = 7;
@@ -71,7 +219,48 @@ int main() {
 	// tamanho
 	cout << "tamanho: " << tamanhoList(lista) << endl;
 	// remover
+	cout << "removendo da posicao 1"<< endl;
 	removerList(lista, 1);
 	printList(lista);
+	// meio da lista
+	cout << "meio: " << meioList(lista) << endl;
+
+	// converter em lista circular
+	circleList(lista);
+	// printar dando duas voltas para testar se realmente está no formato circular
+	cout << "lista circurlar (2 voltas) = " << endl;
+	printCircleListTwoTimes(lista);
+	
+	//criando lista duplamente encadeada
+	noD *listaD = new noD;
+	listaD -> data = 8;
+	listaD -> next = NULL;
+	listaD -> prev = NULL;
+	
+	noD *listaD2 = new noD;
+	listaD2 -> data = 11;
+	listaD2 -> next = NULL;
+	listaD -> next = listaD2;
+	listaD2 -> prev = listaD;
+	
+	noD *listaD3 = new noD;
+	listaD3 -> data = -15;
+	listaD3 -> next = NULL;
+	listaD2 -> next = listaD3;
+	listaD3 -> prev = listaD2;
+	
+	noD *listaD4 = new noD;
+	listaD4 -> data = 10;
+	listaD4 -> next = NULL;
+	listaD3 -> next = listaD4;
+	listaD4 -> prev = listaD3;
+	
+	// exibindo
+	printListD(listaD);
+	// maior no da lista
+	cout << "maior no da lista = " << maiorNoD(listaD) << endl;
+	// ordenando
+	
+	
 	return 0;
 }
